14d0779e05167db5b314c753a871d03cf3c6124c
Aymeric Weinbach <397730+zecloud@users.noreply.github.com>
2026-05-07 18:05:51 +0000

    Add initial Azure Functions project structure and configuration files

---
+++ function_app.py
@@ -0,0 +1,50 @@
+import azure.functions as func
+import datetime
+import json
+import logging
+
+app = func.FunctionApp()
+
+@app.route(route="helloworld", auth_level=func.AuthLevel.ANONYMOUS)
+def helloworld(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    name = req.params.get('name')
+    if not name:
+        try:
+            req_body = req.get_json()
+        except ValueError:
+            pass
+        else:
+            name = req_body.get('name')
+
+    if name:
+        return func.HttpResponse(f"Hello, {name}. This HTTP triggered function executed successfully.")
+    else:
+        return func.HttpResponse(
+             "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.",
+             status_code=200
+        )
+
+
+@app.blob_trigger(arg_name="myblob", path="test",
+                               connection="BlobStorageConnectionString") 
+def reactblobfunc(myblob: func.InputStream):
+    logging.info(f"Python blob trigger function processed blob"
+                f"Name: {myblob.name}"
+                f"Blob Size: {myblob.length} bytes")
+
+
+# This example uses SDK types to directly access the underlying BlobClient object provided by the Blob storage trigger.
+# To use, uncomment the section below and add azurefunctions-extensions-bindings-blob to your requirements.txt file
+# Ref: aka.ms/functions-sdk-blob-python
+#
+# import azurefunctions.extensions.bindings.blob as blob
+# @app.blob_trigger(arg_name="client", path="test",
+#                   connection="BlobStorageConnectionString")
+# def reactblobfunc(client: blob.BlobClient):
+#     logging.info(
+#         f"Python blob trigger function processed blob \n"
+#         f"Properties: {client.get_blob_properties()}\n"
+#         f"Blob content head: {client.download_blob().read(size=1)}"
+#     )
